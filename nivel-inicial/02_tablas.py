ae70dac24c96558343ac0277cf525e2dcde06afb
DaniellaPR <159065844+DaniellaPR@users.noreply.github.com>
2025-07-28 20:43:43 -0500

    Create 02_tablas.py
    Ejercicio 2 tablas

---
+++ nivel-inicial/02_tablas.py
@@ -0,0 +1,11 @@
+#-*-coding:cp1252-*-
+print("TABLAS DE MULTIPLICAR")
+print("------------------------")
+num = int(input("Ingrese un número entre 1 y 12..."))
+print(f"Tabla del {num}")
+if num<13 and num>=1:
+    for i in range(13)
+      print(f"{num} * i = {num*i}")
+else:
+    print("Él número no cumple con las condiciones, no puede construir la tabla de multiplicar")
+print("Gracias por usar el programa")
