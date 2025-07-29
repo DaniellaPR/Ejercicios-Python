ce7b110277d7e05accb5ad2a3a5e6c82b2dd5747
DaniellaPR <159065844+DaniellaPR@users.noreply.github.com>
2025-07-28 21:08:15 -0500

    Create 03_serie.py
    Ejercicio 3 serie de ida y vuelta con un numero

---
+++ nivel-inicial/03_serie.py
@@ -0,0 +1,10 @@
+#-*-coding:1252-*-
+numero = int(input("Hasta que número..."))
+i=0
+while i != numero:
+    print(i+1, end=(""))
+    i += 1
+while numero > 0:
+    print(numero, end=(""))
+    numero -=1
+print("Gracias por usar el programa...")
